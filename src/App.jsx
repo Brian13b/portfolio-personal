9eca4aeba63442a3c55ceb76d2391be4345b1e00
Brian Battauz <brian.nbattauz@gmail.com>
2025-09-16 13:59:32 -0300

    Eliminando dependencias en conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Proyectos from './components/Proyectos';
 import SobreMi from './components/SobreMi';
 import Contacto from './components/Contacto';
 import Footer from './components/Footer';
-import { Helmet } from 'react-helmet-async';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(() => {
@@ -46,16 +45,6 @@ export default function App() {
 
   return (
     <>
-      <Helmet>
-        <title>Brian Battauz | Portfolio</title>
-        <meta name="description" content="Portfolio personal de Brian Battauz, desarrollador Full Stack con experiencia en React, FastAPI y PostgreSQL." />
-        <meta name="keywords" content="Portfolio, React, FastAPI, PostgreSQL, Backend, Frontend" />
-        <meta property="og:title" content="Brian Battauz | Portfolio" />
-        <meta property="og:description" content="Descubre mis proyectos en React, FastAPI, Java, y más." />
-        <meta property="og:image" content="/assets/images/foto_perfil.png" />
-        <meta property="og:url" content="https://tu-dominio.com" />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Helmet>
       
       <Header
         darkMode={darkMode}
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css';
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
       <App />
-    </HelmetProvider>
   </React.StrictMode>
 );
